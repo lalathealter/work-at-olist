a3227a4012ea26b826f60451133b38189fcb52f9
lalathealter <musin.daniiil@yandex.ru>
2023-10-31 18:01:02 +0500

    made a basic implementation of the #2 feature mentioned in README (authors' data exposure on an endpoint)

--- server/main.go
+++ server/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/lalathealter/olist/db"
 )
 
 func main() {
 	fmt.Println("Hello world!")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/authors", func(w http.ResponseWriter, r *http.Request) {
+		dbConn := db.Instance
+
+		queryVals := r.URL.Query()
+		namePattern := queryVals.Get("like")
+		if namePattern == "" {
+			namePattern = "%"
+		}
+
+		rows, err := dbConn.Query(db.SelectAuthorsStmt, namePattern)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		responseSlice := make([]string, 0)
+		for rows.Next() {
+			var name string
+			err := rows.Scan(&name)
+
+			if err != nil {
+				panic(err)
+			}
+			responseSlice = append(responseSlice, name)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(responseSlice)
+	})
+
+	http.ListenAndServe("localhost:5000", mux)
 
 }
