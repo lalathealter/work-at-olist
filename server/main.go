8aff68e0c85c23b16c54bc05d5f0822a922117af
lalathealter <musin.daniiil@yandex.ru>
2023-10-31 19:43:40 +0500

    implemented pagination for the authors endpoint (feature #2 in Readme)

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/lalathealter/olist/db"
 )
@@ -13,35 +14,65 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/authors", func(w http.ResponseWriter, r *http.Request) {
-		dbConn := db.Instance
-
-		queryVals := r.URL.Query()
-		namePattern := queryVals.Get("like")
-		if namePattern == "" {
-			namePattern = "%"
-		}
-
-		rows, err := dbConn.Query(db.SelectAuthorsStmt, namePattern)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		responseSlice := make([]string, 0)
-		for rows.Next() {
-			var name string
-			err := rows.Scan(&name)
-
-			if err != nil {
-				panic(err)
-			}
-			responseSlice = append(responseSlice, name)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(responseSlice)
-	})
+	mux.HandleFunc("/authors", HandleGetAuthors)
 
 	http.ListenAndServe("localhost:5000", mux)
 
+}
+
+type PaginatedArray[T any] struct {
+	Data         []T
+	PageNum      int
+	NextPageLink string
+}
+
+const pageQuery = "page"
+const likeQuery = "like"
+
+func HandleGetAuthors(w http.ResponseWriter, r *http.Request) {
+	dbConn := db.Instance
+
+	queryVals := r.URL.Query()
+	namePattern := queryVals.Get(likeQuery)
+	if namePattern == "" {
+		namePattern = "%"
+	}
+	pageArg := queryVals.Get(pageQuery)
+	pageNum, err := strconv.Atoi(pageArg)
+	if err != nil || pageNum < 1 {
+		pageNum = 1
+		queryVals.Set(pageQuery, "1")
+	}
+
+	rows, err := dbConn.Query(db.SelectAuthorsStmt, namePattern, pageNum)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	responseSlice := make([]string, 0)
+	for rows.Next() {
+		var name string
+		err := rows.Scan(&name)
+
+		if err != nil {
+			panic(err)
+		}
+		responseSlice = append(responseSlice, name)
+	}
+
+	pagRes := PaginatedArray[string]{
+		responseSlice, pageNum, "",
+	}
+	if len(responseSlice) >= db.AuthorsPaginationLimit {
+		if err != nil {
+			panic(err)
+		}
+
+		queryVals.Set(pageQuery, strconv.Itoa(pageNum+1))
+		r.URL.RawQuery = queryVals.Encode()
+		pagRes.NextPageLink = r.Host + r.URL.String()
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pagRes)
 }
