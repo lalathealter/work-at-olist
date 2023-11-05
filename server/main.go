a65014924a6c5b6c6f9dba63bcff22436106c409
lalathealter <musin.daniiil@yandex.ru>
2023-11-05 17:57:46 +0500

    fleshed out a structure for the books POST endpoint

--- server/main.go
+++ server/main.go
@@ -9,15 +9,72 @@ import (
 	"github.com/lalathealter/olist/db"
 )
 
+func HandleMethodNotSupported(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "This endpoint doesn't support the provided method",
+		http.StatusNotImplemented)
+}
+
+type MethodMap map[string]http.HandlerFunc
+
+func HandleEndpointMethods(mm *MethodMap) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler, ok := (*mm)[r.Method]
+		if !ok {
+			HandleMethodNotSupported(w, r)
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
 func main() {
 	fmt.Println("Hello world!")
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/authors", HandleGetAuthors)
+	authorsHandlers := &MethodMap{
+		"GET": HandleGetAuthors,
+	}
+	mux.HandleFunc("/authors", HandleEndpointMethods(authorsHandlers))
 
+	booksHandlers := &MethodMap{
+		"POST": HandlePostBooks,
+	}
+	mux.HandleFunc("/books", HandleEndpointMethods(booksHandlers))
 	http.ListenAndServe("localhost:5000", mux)
 
+}
+
+func HandlePostBooks(w http.ResponseWriter, r *http.Request) {
+	bookObj := db.BookModel{}
+	err := json.NewDecoder(r.Body).Decode(&bookObj)
+	if err != nil {
+		panic(err)
+	}
+
+	if bookObj.Name == "" {
+		http.Error(w, "Book must have a non-empty name",
+			http.StatusBadRequest)
+		return
+	}
+
+	if len(bookObj.Authors) < 1 {
+		http.Error(w, "Book must have at least one author",
+			http.StatusBadRequest)
+		return
+	}
+
+	err = db.SearchForAuthors(bookObj.Authors)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = db.InsertBook(bookObj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 type PaginatedArray[T any] struct {
@@ -63,6 +120,7 @@ func HandleGetAuthors(w http.ResponseWriter, r *http.Request) {
 	pagRes := PaginatedArray[string]{
 		responseSlice, pageNum, "",
 	}
+
 	if len(responseSlice) >= db.AuthorsPaginationLimit {
 		if err != nil {
 			panic(err)
